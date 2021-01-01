1892415ed055fbdf2c87a40ad1e44207aac1ba26
pavan116-g <73816233+pavan116-g@users.noreply.github.com>
2021-01-01 23:14:33 +0530

    Add files via upload

---
+++ Greatest btw three.py
@@ -0,0 +1,10 @@
+#Greatest btw 3 numbers 
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+c=int(input("Enter the third number"))
+if(a>b and a>c):
+    print(a,"is the greatest")
+elif(b>a and b>c):
+    print(b,"Is the greatest")
+else:
+    print(c,"Is the greatest")
