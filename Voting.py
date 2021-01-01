9d1b9046101b4a1792e15ffb8a35dc2e9766555c
pavan116-g <73816233+pavan116-g@users.noreply.github.com>
2021-01-01 23:16:50 +0530

    Add files via upload

---
+++ Voting.py
@@ -0,0 +1,8 @@
+#Eligible for voting or not 
+name=input("Enter your Name")
+age=int(input("Enter the Age"))
+if(age>=18):
+    print(name,"eligible for voting")
+
+else:
+   print(name,"Not eligible for voting")
