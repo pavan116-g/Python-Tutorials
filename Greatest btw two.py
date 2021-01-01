c76b4f68b999ef0983e415668ec8a176a086ea6d
pavan116-g <73816233+pavan116-g@users.noreply.github.com>
2021-01-01 23:11:49 +0530

    Add files via upload

---
+++ Greatest btw two.py
@@ -0,0 +1,7 @@
+#Greatest b/w two numbers
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+if(a>b):
+    print(a,"Is the greatest")
+else:
+   print(b,"Is the greatest")
