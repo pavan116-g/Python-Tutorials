2c418221e688590beb8f241319fccff0d7bf1102
pavan116-g <73816233+pavan116-g@users.noreply.github.com>
2021-01-08 22:30:23 +0530

    Add files via upload

---
+++ prg32.py
@@ -0,0 +1,23 @@
+#Python prg to demonstrate List Collection
+#list: It is a collection of different types of elements
+l=[12,13,14]
+#l
+#l[1]
+#l[2]
+#l[0]
+
+l1=[53,23,65]
+l.append(l1)
+
+
+l.insert(4,50)
+
+l.insert(5,[1,2,3])
+
+
+l2=[1,2,3,4]
+l2=l2*3
+
+
+l3=[8,9]
+l2.extend(l3)
---
+++ prg33.py
@@ -0,0 +1,19 @@
+'''def add():
+    a,b=10,30
+    print(a+b)
+add()
+
+def add(a,b):
+    print(a+b)
+add(20,30)
+'''
+
+'''def add():
+    a,b=10,20
+    return a+b
+print(add())
+'''
+
+def add(x,y):
+    return x+y
+print(add(10,20))
