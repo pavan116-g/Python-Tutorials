01ae413af4f90cc4bdb7a8250e87c3e54e3befe0
pavan116-g <73816233+pavan116-g@users.noreply.github.com>
2021-01-08 22:48:47 +0530

    Add files via upload

---
+++ prg41.py
@@ -0,0 +1,9 @@
+#41 Default argments
+#def add(x,y):
+#    print(x+y)
+#add(2,5)
+
+
+def add(a,b
+
+
---
+++ prg43.py
@@ -0,0 +1,9 @@
+# Factorial of a number using recursion
+
+def recur_factorial(n):
+   if n == 1:
+       return n
+   else:
+       return n*recur_factorial(n-1)
+
+num = 7
